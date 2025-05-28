f4bdcf0338831d87c43b772de32bf2e56296d715
MatinAfrashteh <matinafrashteh992@gmail.com>
2025-05-28 11:50:42 +0330

    Build Model Layer

---
+++ src/main/java/com/example/Technical_Assessment_Assignment/Model/Claim.java
@@ -0,0 +1,30 @@
+package com.example.Technical_Assessment_Assignment.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table
+public class Claim {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int claim_Id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_Id")
+    private endUser endUser;
+
+    @ManyToOne
+    @JoinColumn(name = "lost_Items")
+    private LostItems lostItems;
+
+    private String claimedQuantity;
+}

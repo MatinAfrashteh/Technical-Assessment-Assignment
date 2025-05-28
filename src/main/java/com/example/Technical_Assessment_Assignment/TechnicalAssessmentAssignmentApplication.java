d5462456ebe5a4d0b8e1f324d692122f245143ce
MatinAfrashteh <matinafrashteh992@gmail.com>
2025-05-28 11:45:04 +0330

    Create Project

---
+++ src/main/java/com/example/Technical_Assessment_Assignment/TechnicalAssessmentAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.Technical_Assessment_Assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalAssessmentAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalAssessmentAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Technical_Assessment_Assignment/TechnicalAssessmentAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Technical_Assessment_Assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalAssessmentAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
